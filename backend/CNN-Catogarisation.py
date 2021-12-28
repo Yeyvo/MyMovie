e881a27d48069006a93a5551e4efcde754a6b3cd
AttaouiAhmedAli <attaouiahmedali@gmail.com>
2021-12-28 17:53:18 +0100

    changes in CNN

--- backend/CNN-Catogarisation.py
+++ backend/CNN-Catogarisation.py
@@ -5,53 +5,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
-print(tf.__version__)
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization, Conv2D, MaxPool2D
+from os.path import exists
 
+def createModel(DataShape) :
+  model = Sequential()
+  model.add(Conv2D(16, (3, 3), activation='relu', input_shape=DataShape, kernel_regularizer='l2'))
+  model.add(BatchNormalization())
+  model.add(MaxPool2D(2, 2))
+  model.add(Dropout(0.3))
 
+  model.add(Conv2D(32, (3, 3), activation='relu', kernel_regularizer='l2'))
+  model.add(BatchNormalization())
+  model.add(MaxPool2D(2, 2))
+  model.add(Dropout(0.3))
 
-data = pd.read_csv('C:\\Users\\thex2\\Desktop\\Cours\\Semestre_3\\PI\\Programme\\MyMovie\\backend\\Multi_Label_dataset\\train.csv')
+  model.add(Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l2'))
+  model.add(BatchNormalization())
+  model.add(MaxPool2D(2, 2))
+  model.add(Dropout(0.4))
 
-data.head()
-img_width = 350
-img_height = 350
+  model.add(Conv2D(128, (3, 3), activation='relu', kernel_regularizer='l2'))
+  model.add(BatchNormalization())
+  model.add(MaxPool2D(2, 2))
+  model.add(Dropout(0.5))
 
-X = []
+  model.add(Flatten())
 
-for i in tqdm(range(data.shape[0])):
-  path = 'C:\\Users\\thex2\\Desktop\\Cours\\Semestre_3\\PI\\Programme\\MyMovie\\backend\\Multi_Label_dataset\\Images\\' + data['Id'][i] + '.jpg'
-  img = image.load_img(path, target_size=(img_width, img_height, 3))
-  img = image.img_to_array(img)
-  img = img/255.0
-  X.append(img)
+  model.add(Dense(128, activation='relu', kernel_regularizer='l2'))
+  model.add(BatchNormalization())
+  model.add(Dropout(0.5))
 
-X = np.array(X)
+  model.add(Dense(128, activation='relu', kernel_regularizer='l2'))
+  model.add(BatchNormalization())
+  model.add(Dropout(0.5))
 
-
-plt.imshow(X[2250])
-plt.show()
-
-y = data.drop(['Id', 'Genre'], axis = 1)
-y = y.to_numpy()
-
-X_train , X_test , y_train , y_test =  train_test_split(X,y,random_state = 0,test_size = 0.15)
-print("partie qui marche finie\n")
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-  tf.config.experimental.set_memory_growth(gpu, True)
-print("config done\n")
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
-print("Creation du model\n")
-model.summary()
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-history = model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test))
+  model.add(Dense(25, activation='sigmoid', kernel_regularizer='l2'))
+  model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+  return(model)
 
 def plot_learningCurve(history, epoch):
   # Plot training & validation accuracy values
@@ -72,8 +64,85 @@ def plot_learningCurve(history, epoch):
   plt.xlabel('Epoch')
   plt.legend(['Train', 'Val'], loc='upper left')
   plt.show()
-plot_learningCurve(history, 5)
 
-test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
 
-print('\nTest accuracy:', test_acc)
+
+
+def getModel() :
+  if (exists('./my_model.h5')):
+    model = tf.keras.models.load_model('my_model.h5')
+    model.summary()
+
+  else :
+
+    print(tf.__version__)
+
+    print("import data\n")
+    data = pd.read_csv(
+      'C:\\Users\\thex2\\Desktop\\Cours\\Semestre_3\\PI\\Programme\\MyMovie\\backend\\Multi_Label_dataset\\train.csv')
+    print("import done\n")
+    data.head()
+    img_width = 350
+    img_height = 350
+
+    X = []
+
+    for i in tqdm(range(data.shape[0])):
+      path = 'C:\\Users\\thex2\\Desktop\\Cours\\Semestre_3\\PI\\Programme\\MyMovie\\backend\\Multi_Label_dataset\\Images\\' + \
+             data['Id'][i] + '.jpg'
+      img = image.load_img(path, target_size=(img_width, img_height, 3))
+      img = image.img_to_array(img)
+      img = img / 255.0
+      X.append(img)
+    print("image import and prep to show image")
+    X = np.array(X)
+
+    print("done with images")
+    y = data.drop(['Id', 'Genre'], axis=1)
+    y = y.to_numpy()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.15)
+    print("Training set created\n")
+
+    print("model creation")
+
+    model = createModel(X_train[0].shape)
+
+    model.summary()
+
+    history = model.fit(X_train, y_train, epochs=16, validation_data=(X_test, y_test))
+
+    model.save('my_model.h5')
+
+    plot_learningCurve(history, 10)
+
+    test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+
+    print('\nTest accuracy:', test_acc)
+
+  return model;
+
+def evaluatePoster(model,poster):
+  img_width = 350
+  img_height = 350
+  img = image.load_img(poster,target_size=(img_width, img_height, 3))
+  plt.imshow(img)
+  img = image.img_to_array(img)
+  img = img / 255.0
+  img = img.reshape(1, img_width, img_height, 3)
+  Y_prob = model.predict(img)
+  data = ["Action","Adventure","Animation","Biography","Comedy","Crime","Documentary","Drama","Family","Fantasy","History","Horror","Music","Musical","Mystery","N/A","News","Reality-TV","Romance","Sci-Fi","Short","Sport","Thriller","War","Western"]
+  print(Y_prob)
+
+  top2 = np.argsort(Y_prob[0])[:-3:-1]
+  top2proba = np.sort(Y_prob[0])[:-3:-1]
+  for i in range(2):
+    print(data[top2[i]] , "   :   " , top2proba[i])
+
+
+
+model = getModel()
+evaluatePoster(model,'C:\\Users\\thex2\\Desktop\\Cours\\Semestre_3\\PI\\Programme\\MyMovie\\backend\\sing.jpg')
+
+
+
